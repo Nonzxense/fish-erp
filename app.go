9bc0872845f3ea4dc769c2add9e0b21ce6092855
Nonz007x <nathanon.so00@gmail.com>
2026-05-13 21:30:53 +0700

    feat(log): add log file

--- app.go
+++ app.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"context"
+	"log"
+	"os"
+	"path/filepath"
+
 	"fish/internal/database"
+
 	containerDomain "fish/internal/domain/container"
 	invoiceDomain "fish/internal/domain/invoice"
 	partyDomain "fish/internal/domain/party"
 	transactionDomain "fish/internal/domain/transaction"
 	truckInvoiceDomain "fish/internal/domain/truck_invoice"
+
 	"fish/internal/repository"
+
 	"fish/internal/service/container"
 	"fish/internal/service/invoice"
 	"fish/internal/service/party"
 	"fish/internal/service/transaction"
 	truckinvoice "fish/internal/service/truck_invoice"
-	"os"
 
 	"gorm.io/gorm"
 )
@@ -32,10 +38,43 @@ type App struct {
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	db, _, err := database.NewDB()
+
+	configDir, err := os.UserConfigDir()
 	if err != nil {
 		panic(err)
 	}
+
+	appDir := filepath.Join(configDir, "fish")
+	err = os.MkdirAll(appDir, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	logPath := filepath.Join(appDir, "app.log")
+
+	logFile, err := os.OpenFile(
+		logPath,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0666,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(logFile)
+
+	log.Println("APP STARTING")
+
+	db, _, err := database.NewDB()
+	if err != nil {
+		log.Printf("DB INIT FAILED: %+v", err)
+		panic(err)
+	}
+
+	db = db.Debug()
+
+	log.Println("RUNNING MIGRATIONS")
 
 	err = db.AutoMigrate(
 		&transactionDomain.Transaction{},
@@ -52,9 +91,13 @@ func NewApp() *App {
 		&truckInvoiceDomain.OtherExpense{},
 		&truckInvoiceDomain.CustomerContainerItem{},
 	)
+
 	if err != nil {
+		log.Printf("MIGRATION FAILED: %+v", err)
 		panic(err)
 	}
+
+	log.Println("MIGRATIONS COMPLETE")
 
 	transactionRepo := repository.NewTransactionRepository(db)
 	containerRepo := repository.NewContainerRepository(db)
